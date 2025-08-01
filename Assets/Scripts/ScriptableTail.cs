f5da6fb9cf4fe03fba46fbbbd4fed61f278367d5
Basarâm Ștefan <basaramstefan6@gmail.com>
2025-08-01 17:25:57 +0300

    Added tail settings (scriptable object)

---
+++ Assets/Scripts/ScriptableTail.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableTailSpring", menuName = "Scriptable Objects/ScriptableTailSpring")]
+public class ScriptableTail : ScriptableObject
+{
+    [Header("Spring Joint")]
+    public float frequency = 1.0f;
+
+    [Range(0.0f, 1.0f)]
+    public float dampingRatio = 0.5f;
+
+    [Header("Swinging")]
+    public float SwingForce = 1f;
+    public float NormalJumpForce = 7f;
+    public float GravityMultiplier = 1;
+
+    [Header("Attaching")]
+    public float MaxTailLength = 15f;
+    public float MinTailLength = 2f;
+}
