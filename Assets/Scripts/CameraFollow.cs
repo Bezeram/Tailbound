7bd68262d5fdf3fef30de5f9c3ee943e5887dbf4
Bezeram <basaramstefan6@gmail.com>
2025-08-12 18:43:37 +0300

    Fixed Compiler warning CameraFollow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -66,7 +66,7 @@ public class CameraFollow : MonoBehaviour
 
     public IEnumerator ScreenTransition(Screens newScreen)
     {
-        ScreenWipe wipe = FindObjectOfType<ScreenWipe>();
+        ScreenWipe wipe = FindFirstObjectByType<ScreenWipe>();
         if (wipe != null)
             yield return StartCoroutine(wipe.WipeIn());
 
