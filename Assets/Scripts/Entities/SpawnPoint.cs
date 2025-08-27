db7a5be63f99547051b134f9a251ece1c601ccf2
Basarâm Ștefan <basaramstefan6@gmail.com>
2025-08-28 01:21:38 +0300

    Added SpawnPoint script. Improved editor renderer for ScreenArea

---
+++ Assets/Scripts/Entities/SpawnPoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class SpawnPoint : MonoBehaviour
+{
+    private SpriteRenderer _SpriteRenderer;
+    
+    void OnValidate()
+    {
+        if (transform.parent == null || !transform.parent.TryGetComponent(out _SpriteRenderer))
+            Debug.LogWarning("SpawnPoint object has no ScreenArea parent!");
+    }
+
+    void Update()
+    {
+        if (_SpriteRenderer != null)
+            _SpriteRenderer.enabled = !Application.isPlaying;
+    }
+}
